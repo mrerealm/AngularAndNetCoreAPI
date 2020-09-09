84bfed3cd3a70e041f7e0349d5fe080fcdc6e184
mrerealm <marco@e-realm.net>
2020-09-09 22:49:40 +1000

    created from template

---
+++ AngularAndNetCoreAPI/Controllers/PremiumController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AngularAndNetCoreAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PremiumController : ControllerBase
+    {
+       
+        private readonly ILogger<PremiumController> _logger;
+
+        public PremiumController(ILogger<PremiumController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
